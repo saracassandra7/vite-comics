d4b5e9f980500487ab1ae14f3198989edfe2f0cd
saracassandra7 <saracassandra7@gmail.com>
2022-11-16 16:59:55 +0100

    [add] comics cards

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,12 +1,36 @@
 <script>
+import ComicsCard from './ComicsCard.vue';
+import comics from '../assets/data/dc-comics'
+
 export default {
-  name: 'AppMain'
+  name: 'AppMain',
+  components:{
+    ComicsCard
+  },
+  data(){
+    return{
+      comics
+    }
+  }
 }
 </script>
 
 <template>
   <main>
     <div class="jumbotron"></div>
+    <div class="container">
+      <div class="btn">
+        <h3>CURRENT SERIES</h3>
+      </div>
+
+      <div class="comics">
+        <ComicsCard v-for="(comic, index) in comics" :key="index" :comicTitle="comic.series" :comicImg="comic.thumb"/>
+
+      </div>
+
+    </div>
+
+
   </main>
 </template>
 
@@ -15,12 +39,19 @@ export default {
 @use '../styles/partials/variables' as *;
 
 main{
+  background-color: #1c1c1c;
+  color: #f5e8fc;
 
   .jumbotron{
     height: 350px;
     background-image: url(../src/assets/img/jumbotron.jpg);
     background-repeat: no-repeat;
     background-size: cover;
+  }
+
+  .comics{
+    display: flex;
+    flex-wrap: wrap;
   }
 }
 
---
+++ src/components/ComicsCard.vue
@@ -0,0 +1,50 @@
+<script>
+
+export default {
+  name: 'ComicsCard',
+
+  props:{
+    comicImg: String,
+    comicTitle: String
+  }
+}
+</script>
+
+<template>
+<div class="card">
+  <div class="card-img">
+    <img :src="comicImg" :alt="comicTitle">
+  </div>
+  
+  <span>{{comicTitle}}</span>
+</div>
+  
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/mixin' as *;
+@use '../styles/partials/variables' as *;
+
+.card{
+  width: calc(100% / 6 - 5px);
+  margin-bottom: 20px;
+
+  .card-img{
+    width: 150px;
+    height: 150px;
+    overflow: hidden;
+
+    img{
+      max-width: 100%;
+    }
+  }
+
+  span{
+    text-transform: uppercase;
+    display: inline-block;
+    margin: 15px 0;
+    font-size: .9rem;
+  }
+}
+
+</style>
